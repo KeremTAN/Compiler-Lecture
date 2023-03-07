864f02f7edd0737448ad3042956fd995fc1c273f
KeremTAN <keremtan.95@hotmail.com>
2023-03-07 12:34:45 +0300

    BinaryOp returns are fixed and CMakeList is changed

--- src/Front-End/Parsing/BinaryOp.cpp
+++ src/Front-End/Parsing/BinaryOp.cpp
@@ -3,10 +3,10 @@
 BinaryOp::BinaryOp(Operator Op, Expr* Left, Expr* Right)
     :m_Operator(Op), m_Left(Left), m_Right(Right) {}
 
-Expr* BinaryOp::getLeft() { m_Left; }
+Expr* BinaryOp::getLeft() {return  m_Left; }
 
-Expr* BinaryOp::getRight() { m_Right; }
+Expr* BinaryOp::getRight() { return m_Right; }
 
-BinaryOp::Operator BinaryOp::getOperator() { m_Operator; }
+BinaryOp::Operator BinaryOp::getOperator() { return m_Operator; }
 
 void BinaryOp::accept(ASTVisitor& V) { V.visit(*this); }
